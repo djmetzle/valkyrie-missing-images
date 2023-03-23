42c9a83755cdc970fd4c65a101ff23cd7dc9763b
Daryl Metzler <daryl@ifixit.com>
2023-03-23 13:30:57 -0700

    Update to crawl pages+media

---
+++ api-check.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -w
+
+require 'fileutils'
+
+require_relative "./lib/valkyrie-api"
+
+require 'json'
+
+api = ValkyrieAPI.new
+
+#post_links = api.fetch_post_list()
+#puts "Fetched #{post_links.size} posts"
+
+#page_links = api.fetch_page_list()
+#puts "Fetched #{page_links.size} pages"
+
+#media_links = api.fetch_media_list()
+#puts "Fetched #{media_links.size} media items"
--- lib/valkyrie-api.rb
+++ lib/valkyrie-api.rb
@@ -2,13 +2,19 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-ENDPOINT = 'https://54.71.223.11/wp-json/wp/v2/posts'
+API_BASE = 'https://54.71.223.11/wp-json/wp/v2'
+POSTS_ENDPOINT = API_BASE + '/posts'
+PAGES_ENDPOINT = API_BASE + '/pages'
+MEDIA_ENDPOINT = API_BASE + '/media'
 
 PER_PAGE = 100
 
 POST_FIELDS = 'id,title,link'
+PAGE_FIELDS = 'id,title,link'
 
 PostLink = Struct.new('PostLink', :id, :link, :title)
+PageLink = Struct.new('PageLink', :id, :link, :title)
+MediaLink = Struct.new('PageLink', :id, :link, :title)
 
 class ValkyrieAPI
   def initialize()
@@ -25,26 +31,62 @@ class ValkyrieAPI
     i = 1
     all_posts = []
     while true
-       posts = get_page(i)
+       posts = get_page(i, POSTS_ENDPOINT)
        i += 1
        break if posts.nil?
        STDERR.puts "Fetched #{posts.size} posts from API"
        all_posts.push(*posts)
     end
-    return all_posts.map { |post| 
+    return all_posts.map { |post|
         to_struct(post)
       }.select { |post|
         is_valkyrie_post?(post.link)
       }
   end
 
-  def get_page(n)
-     uri = URI.parse(ENDPOINT)
+  def fetch_page_list()
+    i = 1
+    all_pages = []
+    while true
+       pages = get_page(i, PAGES_ENDPOINT)
+       i += 1
+       break if pages.nil?
+       STDERR.puts "Fetched #{pages.size} pages from API"
+       all_pages.push(*pages)
+    end
+    return all_pages.map { |page|
+        to_page_struct(page)
+      }
+
+  end
+
+  def fetch_media_list()
+    i = 1
+    all_medias = []
+    while true
+       medias = get_page(i, MEDIA_ENDPOINT)
+       i += 1
+       break if medias.nil?
+       STDERR.puts "Fetched #{medias.size} medias from API"
+       all_medias.push(*medias)
+    end
+    return all_medias.map { |media|
+        to_media_struct(media)
+      }
+
+  end
+
+  def get_page(n, endpoint)
      params = {
         :page => n,
         :per_page => PER_PAGE,
         :_fields => POST_FIELDS,
      }
+     return fetch_endpoint(endpoint, params)
+  end
+
+  def fetch_endpoint(endpoint, params)
+     uri = URI.parse(endpoint)
      uri.query = URI.encode_www_form(params)
      request = Net::HTTP::Get.new(uri)
      request["Authorization"] = basic_auth()
@@ -62,12 +104,23 @@ class ValkyrieAPI
      if response.code != "200"
         return nil
      end
+
      return JSON.parse(response.body)
   end
 
   def to_struct(post)
     post_struct = PostLink.new(post["id"], post["link"], post["title"]["rendered"])
     return post_struct
+  end
+
+  def to_page_struct(page)
+    page_struct = PageLink.new(page["id"], page["link"], page["title"]["rendered"])
+    return page_struct
+  end
+
+  def to_media_struct(media)
+    media_struct = MediaLink.new(media["id"], media["link"], media["title"]["rendered"])
+    return media_struct
   end
 
   def is_valkyrie_post?(link)
